2d961a002b1b3710f25b9e25efcf31d03e6dbd05
Tianyu Ding <tianyuding@uvic.ca>
2025-12-28 16:49:15 -0800

    data format complete

--- dags/kafka_stream.py
+++ dags/kafka_stream.py
@@ -10,10 +10,37 @@ default_args = {
     'start_date': datetime(2025, 12, 27),
 }
 
-def stream_data():
+def get_data():
+    """
+    this function gets data from the API, and returns the clean data (result only)
+    """
     request = requests.get("https://randomuser.me/api/")
     raw_data = request.json()
     data = raw_data['results'][0]
+    return data 
+
+def format_data(data):
+    """
+    this function formats the data into a json string
+    """
+    formatted_data = {}
+    formatted_data['first_name'] = data['name']['first']
+    formatted_data['last_name'] = data['name']['last']
+    formatted_data['gender'] = data['gender']
+    formatted_data['address'] = f"{data['location']['street']['number']} {data['location']['street']['name']}, {data['location']['city']}, {data['location']['state']}, {data['location']['country']}, {data['location']['postcode']}"
+    formatted_data['postcode'] = data['location']['postcode']
+    formatted_data['email'] = data['email']
+    formatted_data['username'] = data['login']['username']
+    formatted_data['date_of_birth'] = data['dob']['date']
+    formatted_data['age'] = data['dob']['age']
+    formatted_data['phone'] = data['phone']
+    formatted_data['picture'] = data['picture']['large']
+
+    return json.dumps(formatted_data, indent=4)
+
+def stream_data():
+    data = get_data()
+    data = format_data(data)
     print(data)
 
 # with DAG(
