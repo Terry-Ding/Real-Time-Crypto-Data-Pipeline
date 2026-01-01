c4a6c01968d96c82f7986467eede88fd94443db6
Tianyu Ding <tianyuding@uvic.ca>
2026-01-01 15:52:46 -0800

    code framework finished

--- spark_stream.py
+++ spark_stream.py
@@ -4,3 +4,18 @@ from cassandra.auth import PlainTextAuthProvider
 from cassandra.cluster import Cluster
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import from_json, col 
+
+def create_keyspace(session):
+    """ this function creates keyspace """
+
+def create_table(session):
+    """ this function creates table """
+
+def insert_data(session, **kwargs):
+    """ this function inserts data from kafka """
+
+def create_spark_connection():
+    """ this function creates spark connection """
+
+def create_cassandra_connection():
+    """ this function creates cassandra connection """
